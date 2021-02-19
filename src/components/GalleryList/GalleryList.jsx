5d0be32dce5e77f173d134e04f2032224147ef71
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-18 22:29:00 -0600

    imported style sheet for GalleryList to resize images

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,17 +1,19 @@
+import './GalleryList.css'
+
 function GalleryList({galleryList}) {
   return(
-    <main>
+    <main class="GalleryList-main">
       {galleryList.map(image => {
         return(
-          <div key={image.id}>
+          <div class="GalleryList-container" key={image.id}>
             {/* Img src pulled from gallery.data - axios: /GET */}
-            <img src={image.path}/>
+            <img class="GalleryList-image" src={image.path}/>
             {/* Description pulled from gallery.data - axios: /GET */}
-            <p>{image.description}</p>
+            <p class="GalleryList-description">{image.description}</p>
             {/* Like button send to gallery.data - axios: /PUT */}
             <button>Love it!</button>
             {/* Number of likes pulled from gallery.data - axios: /GET */}
-            <p>{image.likes}</p>
+            <p class="GalleryList-likes">{image.likes}</p>
           </div>
         ); // end return
       })}
