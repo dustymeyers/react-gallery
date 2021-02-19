c9095868d23e5d64748a085ac732b7e9298a45c9
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-19 16:43:27 -0600

    Conditional rendering on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,23 @@
+import {useState} from 'react';
 import './GalleryItem.css';
 
 function GalleryItem({image}) {
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    // on click change the boolean state of isClick 
+    setIsClicked(!isClicked);
+  };
+
   return(
     <div className="GalleryItem">
-      {/* Img src pulled from gallery.data - axios: /GET */}
-      <img className="GalleryItem-image" src={image.path}/>
-      {/* Description pulled from gallery.data - axios: /GET */}
-      <p className="GalleryItem-description">{image.description}</p>
+      {isClicked 
+      // isClicked = true: show image description 
+      ? <p onClick={handleClick} className="GalleryItem-description">{image.description}</p>
+      // isClicked = false: show image (default)
+      : <img onClick={handleClick} className="GalleryItem-image" src={image.path}/>
+      }
       {/* Like button send to gallery.data - axios: /PUT */}
       <button>Love it!</button>
       {/* Number of likes pulled from gallery.data - axios: /GET */}
