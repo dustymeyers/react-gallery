675b16bb33b1364b500f08be7443686f4d5bad3d
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-18 21:47:16 -0600

    setting up fetchGallery, logs data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   // Set a state to contain GalleryListData
-  // const [galleryList, setGalleryList] = useState([]);
+  const [galleryList, setGalleryList] = useState([]);
+
+  // On page load
+  useEffect(() => {
+    // call GET function
+    fetchGallery();
+  });// end useEffect
+
+  // Axios /GET call
+  const fetchGallery = () => {
+    // Point server at /gallery using GET
+    axios.get('/gallery')
+      // send back data
+      .then(res => console.log('GET gallery data', res))
+  }; // end fetchGallery
+
 
     return (
       <div className="App">
@@ -17,7 +33,7 @@ function App() {
             <img src="images/goat_small.jpg"/>
             {/* Description pulled from gallery.data - axios: /GET */}
             <p>Photo of a goat taken at Glacier National Park.</p>
-            {/* Like button */}
+            {/* Like button send to gallery.data - axios: /PUT */}
             <button>Love it!</button>
             {/* Number of likes pulled from gallery.data - axios: /GET */}
             <p>4 people love this!</p>
