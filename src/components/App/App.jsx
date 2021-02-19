7902b8128388c14bd96c42127bca19aabf101e80
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-18 21:32:20 -0600

    built out initial framework in app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // Set a state to contain GalleryListData
+  // const [galleryList, setGalleryList] = useState([]);
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Goat Gallery</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* Example of a List Item */}
+        <main>
+          <div>
+            {/* Img src pulled from gallery.data - axios: /GET */}
+            <img src="images/goat_small.jpg"/>
+            {/* Description pulled from gallery.data - axios: /GET */}
+            <p>Photo of a goat taken at Glacier National Park.</p>
+            {/* Like button */}
+            <button>Love it!</button>
+            {/* Number of likes pulled from gallery.data - axios: /GET */}
+            <p>4 people love this!</p>
+          </div>
+        </main>
       </div>
     );
 }
