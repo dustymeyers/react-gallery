6aa0090eed08de51a8c86cbb0c595920fac1973d
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-19 08:45:25 -0600

    added GalleryItem style sheet and updated styling for new component

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,15 +1,16 @@
+import './GalleryItem.css';
 
 function GalleryItem({image}) {
   return(
-    <div className="GalleryList-image-container">
+    <div className="GalleryItem">
       {/* Img src pulled from gallery.data - axios: /GET */}
-      <img className="GalleryList-image" src={image.path}/>
+      <img className="GalleryItem-image" src={image.path}/>
       {/* Description pulled from gallery.data - axios: /GET */}
-      <p className="GalleryList-description">{image.description}</p>
+      <p className="GalleryItem-description">{image.description}</p>
       {/* Like button send to gallery.data - axios: /PUT */}
       <button>Love it!</button>
       {/* Number of likes pulled from gallery.data - axios: /GET */}
-      <p className="GalleryList-likes">{image.likes}</p>
+      <p className="GalleryItem-likes">{image.likes}</p>
     </div>
   ); // end return
 } // end GalleryItem
