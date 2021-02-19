d9aa26642df9d06d5720623be7b51214cff8f75c
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-19 08:40:38 -0600

    moved Gallery Item into it's own component

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,17 @@
-function GalleryItem() {
 
+function GalleryItem({image}) {
+  return(
+    <div className="GalleryList-image-container">
+      {/* Img src pulled from gallery.data - axios: /GET */}
+      <img className="GalleryList-image" src={image.path}/>
+      {/* Description pulled from gallery.data - axios: /GET */}
+      <p className="GalleryList-description">{image.description}</p>
+      {/* Like button send to gallery.data - axios: /PUT */}
+      <button>Love it!</button>
+      {/* Number of likes pulled from gallery.data - axios: /GET */}
+      <p className="GalleryList-likes">{image.likes}</p>
+    </div>
+  ); // end return
 } // end GalleryItem
 
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,20 +1,16 @@
-import './GalleryList.css'
+import GalleryItem from '../GalleryItem/GalleryItem'
+import './GalleryList.css';
+
 
 function GalleryList({galleryList}) {
   return(
     <main className="GalleryList">
       {galleryList.map(image => {
         return(
-          <div className="GalleryList-image-container" key={image.id}>
-            {/* Img src pulled from gallery.data - axios: /GET */}
-            <img className="GalleryList-image" src={image.path}/>
-            {/* Description pulled from gallery.data - axios: /GET */}
-            <p className="GalleryList-description">{image.description}</p>
-            {/* Like button send to gallery.data - axios: /PUT */}
-            <button>Love it!</button>
-            {/* Number of likes pulled from gallery.data - axios: /GET */}
-            <p className="GalleryList-likes">{image.likes}</p>
-          </div>
+          <GalleryItem 
+            key={image.id}
+            image={image}
+          />
         ); // end return
       })}
     </main>
